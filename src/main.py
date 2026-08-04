e3ba560d5122d45b3f1257a34dd3520828ee7f35
John Kasisi <jngatia816@gmail.com>
2026-08-04 14:36:39 +0300

    Create project architecture and first renderer program

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def main():
+    print("====================================")
+    print("      THE RENDERER v0.1")
+    print("====================================")
+    print("Status : Online")
+    print("Domain : Checkers")
+    print("Output : Ready")
+    print()
+    print("Welcome to The Renderer.")
+
+
+if __name__ == "__main__":
+    main()
