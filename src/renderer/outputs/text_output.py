6934ca980e262978cc978707dbc5588b4547b529
John Kasisi <jngatia816@gmail.com>
2026-08-05 10:32:53 +0300

    still at png

--- src/renderer/outputs/text_output.py
+++ src/renderer/outputs/text_output.py
@@ -1,13 +1,16 @@
 from src.renderer.board_renderer import BoardRenderer
+from src.renderer.outputs.image_output import ImageOutput
 
 
 class TextOutput:
     """
-    Displays the current game state as a board.
+    Displays the current game state as a board and saves a PNG frame.
     """
 
     def __init__(self):
         self.board_renderer = BoardRenderer()
+        self.image_output = ImageOutput()
+        self.frame_counter = 1
 
 
     def display(self, game_state):
@@ -19,3 +22,12 @@ class TextOutput:
             None,
             game_state.pieces
         )
+
+        filename = f"frame{self.frame_counter:04d}.png"
+
+        self.image_output.export(
+            game_state.pieces,
+            filename
+        )
+
+        self.frame_counter += 1
