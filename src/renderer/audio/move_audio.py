8f0f627411cc45ecfb47041025709592a4ae311e
John Kasisi <jngatia816@gmail.com>
2026-08-06 20:55:10 +0300

    audio done

--- src/renderer/audio/move_audio.py
+++ src/renderer/audio/move_audio.py
@@ -1,6 +1,16 @@
 from src.renderer.audio.audio_map import AUDIO_MAP
 
 
+def square_to_audio(square):
+
+    if square not in AUDIO_MAP:
+
+        return None
+
+    return f"assets/audio/{AUDIO_MAP[square]}"
+
+
+
 def move_to_audio(move):
 
     start, end = move.split("-")
@@ -8,7 +18,22 @@ def move_to_audio(move):
     start = int(start)
     end = int(end)
 
-    return [
-        f"assets/audio/{AUDIO_MAP[start]}",
-        f"assets/audio/{AUDIO_MAP[end]}"
-    ]
+
+    audio_sequence = []
+
+
+    start_audio = square_to_audio(start)
+
+    if start_audio:
+        audio_sequence.append(start_audio)
+
+
+
+    end_audio = square_to_audio(end)
+
+    if end_audio:
+        audio_sequence.append(end_audio)
+
+
+
+    return audio_sequence
