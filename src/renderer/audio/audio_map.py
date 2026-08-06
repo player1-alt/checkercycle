1d0be749a6268dc6cca06a2220121b7750cf5e71
John Kasisi <jngatia816@gmail.com>
2026-08-06 20:23:23 +0300

    audiomap json

--- src/renderer/audio/audio_map.py
+++ src/renderer/audio/audio_map.py
@@ -1,4 +1,20 @@
-AUDIO_MAP = {
-    25: "test.mp3",
-    29: "test.mp3",
-}
+import json
+
+
+CONFIG_FILE = "config/audio_map.json"
+
+
+def load_audio_map():
+
+    with open(CONFIG_FILE, "r") as file:
+
+        data = json.load(file)
+
+    return {
+        int(square): song
+        for square, song in data.items()
+        if song
+    }
+
+
+AUDIO_MAP = load_audio_map()
